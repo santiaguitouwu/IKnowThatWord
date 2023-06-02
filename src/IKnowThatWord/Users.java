2120c028a471634b2c10d348321e2abc2b1b6656
santiaguitouwu <santiago.abihatar.jimenez@correounivalle.edu.co>
2023-06-02 16:23:46 -0500

    Simplificando FileManager - Agregando clase model

--- src/IKnowThatWord/Users.java
+++ src/IKnowThatWord/Users.java
@@ -7,14 +7,14 @@ import java.util.Random;
  *Read the file Users.txt with FileManagerUsers
  */
 public class Users {
-    private ArrayList<String> users = new ArrayList<String>();
+    private ArrayList<String> users;
 
     /**
      * Read the file Users.txt
      */
     public Users(){
-        FileManagerUsers fileManagerUsers = new FileManagerUsers();
-        users = fileManagerUsers.read();
+        FileManager fileManager = new FileManager();
+        users = fileManager.read("src/Files/Users.txt");
     }
 
     /**
