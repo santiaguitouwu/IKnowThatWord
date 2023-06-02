dde1f5508e807deb78f8d2ac365717c1ab8cc54a
scardinaux1 <sarah.cardinaux@correounivalle.edu.co>
2023-06-02 12:42:01 -0500

    creando interfaz

---
+++ src/IKnowThatWord/Interfaz/src/myProject/Header.java
@@ -0,0 +1,22 @@
+package myProject;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Header extends JLabel {
+    /**
+     * Constructor of the Header class
+     * @param title String that contains Header text
+     * @param colorBackground Color object to be assigned for the Header background
+     */
+    public Header(String title, Color colorBackground){
+        this.setText(title);
+        this.setBackground(colorBackground);
+        this.setForeground(new Color(255,255,255));
+        this.setFont(new Font(Font.DIALOG,Font.BOLD,20));
+        this.setHorizontalAlignment(JLabel.CENTER);
+        this.setVerticalAlignment(JLabel.CENTER);
+        this.setOpaque(true);
+    }
+}
