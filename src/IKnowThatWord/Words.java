4ae4143f717a1a7a55b98164cd4b41ed7482e665
scardinaux1 <sarah.cardinaux@correounivalle.edu.co>
2023-06-02 21:51:39 -0500

    update method get words

--- src/IKnowThatWord/Words.java
+++ src/IKnowThatWord/Words.java
@@ -6,15 +6,24 @@ import java.util.Random;
 public class Words {
     private ArrayList<String> words;
     public Words(){
-        FileManager fileManager = new FileManager();
-        words = fileManager.read("src/Files/Words.txt");
+        //FileManager fileManager = new FileManager();
+        //words = fileManager.read("src/Files/Words.txt");
     }
     public String getRamdonWord(){
         Random random = new Random();
         return words.get(random.nextInt(words.size()));
     }
 
-    public ArrayList<String> getWords() {
+    public String[] getWords(int level) {
+        String[] words;
+        switch (level){
+            case 1:
+                words = new String[] {"hola","como","pero","cosa","televisor","casa","carro","moto","piso","pedo"} ;
+                break;
+            default:
+                words = new String[0]; // Valor por defecto si no se cumple ninguno de los casos
+                break;
+        }
         return words;
     }
 }
