3cdf6cd8eda16f3603d597545f9186de95db13b5
scardinaux1 <sarah.cardinaux@correounivalle.edu.co>
2023-06-02 21:50:01 -0500

    create levels class

--- src/IKnowThatWord/Levels.java
+++ src/IKnowThatWord/Levels.java
@@ -1,2 +1,23 @@
-package IKnowThatWord;public class Levels {
+package IKnowThatWord;
+
+public class Levels {
+    private int level = 1;
+
+    public Levels(int level) {
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    /*public void setLevel(int level){
+        this.level = level;
+    }*/
+
+    public int nextLevel() {
+        int currentLevel = getLevel();
+        this.level = currentLevel++;
+        return getLevel();
+    }
 }
